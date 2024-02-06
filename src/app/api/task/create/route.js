47e02f5c5b78d1cc0ac96ef04de863831f8673dc
balinux <ryojuniyantara@gmail.com>
2024-02-06 17:31:34 +0000

    update: route create user tasl

--- src/app/api/task/create/route.js
+++ src/app/api/task/create/route.js
@@ -2,17 +2,41 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/lib/prisma";
 
-export async function POST() {
-  const tasks = await prisma.task.findMany();
-  console.log('tasks', tasks)
-  return NextResponse.json(
-    {
-      sucess: true,
-      message: "List Data Posts",
-      data: tasks,
-    },
-    {
-      status: 200,
-    }
-  )
+export const POST = async (request) => {
+  const { userId, taskId } = await request.json()
+  try {
+    // menandai task sudah dikerjakan
+    const completedTask = await prisma.task.update({
+      where: { id: taskId },
+      data: { completed: true }
+    })
+    console.log("completedTask", completedTask)
+
+    // membuat hubungan m - m antara user dan task
+    const createUserTask = await prisma.userTask.create({
+      data: {
+        userId,
+        taskId
+      }
+    })
+
+    console.log("create usertask", createUserTask)
+
+    // Ambil informasi tugas
+    const tasks = await prisma.task.findUnique({
+      where: { id: taskId }
+    });
+    
+    console.log("tasks", tasks)
+    return NextResponse.json(tasks)
+  } catch (error) {
+    console.error("Error completing task:", error);
+
+    //return NextResponse.json({ error: "Internal Server Error" });
+    return NextResponse.json({ error });
+  }
+  console.log(body)
+  //const userTaskList = await prisma.userTask.create({
+
+  //})
 }
