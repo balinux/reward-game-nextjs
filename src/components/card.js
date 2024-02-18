cde4baddd6ae1373526c6c5ee48d3699227c19d5
balinux <ryojuniyantara@gmail.com>
2024-02-18 11:35:01 +0800

    update: memperbaiki card dan modal

--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,37 @@
-'use client'
+"use client";
+import { useState } from "react";
 
 const Card = ({ data }) => {
+  const [isModalOpen, setIsmodalOpen] = useState(false);
 
   const claimReward = (task) => {
-    console.log("berhasil menambahkan reward")
-    console.log(task)
-    document.getElementById('my_modal_3').close()
-  }
+    console.log("berhasil menambahkan reward");
+    console.log(task);
+    // document.getElementById("my_modal_3").close();
+  };
 
   return (
     <>
       <div className="carousel-item card card-compact w-80 bg-base-100 shadow-xl">
-        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
         <div className="card-body">
-          <h2 className="card-title">{data.title}
+          <h2 className="card-title">
+            {data.title}
             <div className="badge badge-primary">{data.points} ⭐</div>
           </h2>
           <p>{data.description ?? "tidak ada deskripsi"}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-block btn-primary text-white" onClick={() => document.getElementById('my_modal_3').showModal()}>Ambil bintang</button>
+            <button
+              className="btn btn-block btn-primary text-white"
+              onClick={() => setIsmodalOpen(true)}
+            >
+              Ambil bintang
+            </button>
 
             {/* <button className="btn btn-primary">Buy Now</button> */}
           </div>
@@ -27,19 +40,40 @@ const Card = ({ data }) => {
 
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
       {/* <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>open modal</button> */}
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-          </form>
-          <h3 className="font-bold text-lg">Kamu yakin?</h3>
-          <p className="py-4">kamu yakin sudah mengerjakan tugas: <span className="font-bold" >{data.title ?? "tidak ada judul"}</span> </p>
-          <button className="btn btn-block btn-primary text-white" onClick={() => claimReward(data.title)}> Gasss!!!!</button>
-        </div>
-      </dialog>
+      {isModalOpen && (
+        <dialog id="my_modal_3" className="modal" open>
+          <div className="modal-box">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                onClick={() => setIsmodalOpen(false)}
+              >
+                ✕
+              </button>
+            </form>
+            <h3 className="font-bold text-lg">Kamu yakin?</h3>
+            <p className="py-4">
+              kamu yakin sudah mengerjakan tugas:{" "}
+              <span className="font-bold">
+                {data.title ?? "tidak ada judul"}
+              </span>{" "}
+            </p>
+            <button
+              className="btn btn-block btn-primary text-white"
+              onClick={() => {
+                claimReward(data);
+                setIsmodalOpen(false);
+              }}
+            >
+              {" "}
+              Gasss!!!!
+            </button>
+          </div>
+        </dialog>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default Card;
