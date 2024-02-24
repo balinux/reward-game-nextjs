a0101c7c9c271fad7d996b538700261cbb85b742
balinux <ryojuniyantara@gmail.com>
2024-02-24 16:24:06 +0800

    update: menampilkan task berstatus completed: false

--- src/app/api/task/route.js
+++ src/app/api/task/route.js
@@ -3,16 +3,21 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET() {
-    const tasks = await prisma.task.findMany();
-    console.log('tasks', tasks)
-    return NextResponse.json(
-        {
-            sucess: true,
-            message: "List Data Posts",
-            data: tasks,
-        },
-        {
-            status: 200,
-        }
-    )
+  const tasks = await prisma.task.findMany({
+    where: {
+      completed: false,
+    },
+  });
+  console.log("tasks", tasks);
+  return NextResponse.json(
+    {
+      sucess: true,
+      message: "List Data Posts",
+      data: tasks,
+    },
+    {
+      status: 200,
+    },
+  );
 }
+
